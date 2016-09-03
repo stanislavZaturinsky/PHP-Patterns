02b32debfa799263a5c2887645ab220af4063c6e
Stanislav <ctacbok@ukr.net>
2016-09-03 18:16:23 +0400

    test

---
+++ Inheritance/ShopProductFinish.php
@@ -0,0 +1,96 @@
+<?php
+class ShopProduct {
+    private $title;
+    private $producerMainName;
+    private $producerFirstName;
+    protected $price;
+    private $discount = 0;
+
+    public function __construct($title, $firstName,
+                         $mainName, $price) {
+        $this->title             = $title;
+        $this->producerFirstName = $firstName;
+        $this->producerMainName  = $mainName;
+        $this->price             = $price;
+    }
+
+    public function getProducerFirstName() {
+        return $this->producerFirstName;
+    }
+
+    public function getProducerMainName() {
+        return $this->producerMainName;
+    }
+
+    public function setDiscount($num) {
+        $this->discount = $num;
+    }
+
+    public function getDiscount() {
+        return $this->discount;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getPrice() {
+        return ($this->price - $this->discount);
+    }
+
+    function getProducer() {
+        return "{$this->producerFirstName} "
+             . "{$this->producerMainName}";
+
+    }
+
+    function getSummaryLine() {
+        $base  = "$this->title ({$this->producerMainName},";
+        $base .= "{$this->producerFirstName} )";
+        return $base;
+    }
+}
+
+class CDProduct extends ShopProduct {
+    private $playLength = 0;
+
+    public function __construct($title, $firstName,
+                         $mainName, $price, $playLength) {
+        parent::construct($title, $firstName, $mainName, $price);
+        $this->playLength = $playLength;
+    }
+
+    public function getPlayLength() {
+        return $this->playLength;
+    }
+
+    public function getSummaryLine() {
+        $base  = parent::getSummaryLine();
+        $base .= ": Время звучания - {$this->playLength}";
+        return $base;
+    }
+}
+
+class BookProduct extends ShopProduct {
+    private $numPages = 0;
+
+    function __construct($title, $firstName,
+                         $mainName, $price, $numPages) {
+        parent::construct($title, $firstName, $mainName, $price);
+        $this->numPages = $numPages;
+    }
+
+    public function getNumberOfPages() {
+        return $this->numPages;
+    }
+
+    public function getSummaryLine() {
+        $base  = parent::getSummaryLine();
+        $base .= ": {$this->numPages} стр.";
+        return $base;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+}
